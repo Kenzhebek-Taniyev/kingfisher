d494d1de7bbc3d1b8260e3691afe92847f339fb3
Kenzhebek-Taniyev <92087320+Kenzhebek-Taniyev@users.noreply.github.com>
2021-10-07 20:44:11 +0600

    Delete locators.py

--- locators.py
+++
@@ -1,6 +0,0 @@
-class locators:
-    parent = 'div.goodsBlocks'
-    child = 'div.goodsBlock'
-    NAME = 'div.goodsBlock span.wrapperPad a.title'
-    PRICE = 'div.goodsBlock span.wrapperPad span.pricePosition span.price span.new'
-    # CITY = 'span.wrapperPad'
