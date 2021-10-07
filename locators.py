c937d25b9af3b136b33da3fe35aa7c245636a1b7
Kenzhebek-Taniyev <92087320+Kenzhebek-Taniyev@users.noreply.github.com>
2021-10-07 20:35:42 +0600

    Create locators.py

---
+++ locators.py
@@ -0,0 +1,6 @@
+class locators:
+    parent = 'div.goodsBlocks'
+    child = 'div.goodsBlock'
+    NAME = 'div.goodsBlock span.wrapperPad a.title'
+    PRICE = 'div.goodsBlock span.wrapperPad span.pricePosition span.price span.new'
+    # CITY = 'span.wrapperPad'
